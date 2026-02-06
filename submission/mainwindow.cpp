a8ce8b056b10c4adcf20243a54c6862833ebad62
Slava <viavhaslaukandratsenka@gmail.com>
2026-02-06 23:30:15 +0300

    Update mainwindow.cpp

--- submission/mainwindow.cpp
+++ submission/mainwindow.cpp
@@ -23,6 +23,24 @@ MainWindow::MainWindow(QWidget* parent)
     connect(ui->btn_eight,  &QPushButton::clicked, this, &MainWindow::numbers_clicked);
     connect(ui->btn_nine,   &QPushButton::clicked, this, &MainWindow::numbers_clicked);
 
+    QApplication::setStyle("Fusion");
+
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(25, 25, 25));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+    qApp->setPalette(darkPalette);    
 }
 
 MainWindow::~MainWindow() {    // деструктор
